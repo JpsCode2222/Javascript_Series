5be92a902e0fa7cbecf74e20d8c2f728d381ff5f
Jayad Pathan <jayadpathandev@gmail.com>
2024-11-10 21:04:15 +0530

    generate html for order summary

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,5 @@
 // way to import varible in outsorce file
-import {cart, addToCart} from '../data/cart.js';
+import {cart, addToCart, updateCartQuantity} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = '';
